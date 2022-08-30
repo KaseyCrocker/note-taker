0cef54306058e622312efffe0bbe63b371f45747
Kasey <kasey_crocker@yahoo.com>
2022-08-30 15:45:47 -0400

    assets not working

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -3,11 +3,15 @@ const path = require('path');
 
 module.exports = (app) => {
 
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+  });
+
   app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/notes.html'));
   });
 
-  app.get('/', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/index.html'));
   })
 };
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
