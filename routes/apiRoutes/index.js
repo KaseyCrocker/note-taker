db67d1ac51ac4e01d9176b784d142f78906c27a2
Kasey <kasey_crocker@yahoo.com>
2022-08-30 15:02:11 -0400

    create note route fix

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,7 +1,27 @@
 const path = require('path');
 const fs = require('fs')
+const allNotes = require('../../db/db.json');
 
 var uniqid = require('uniqid');
+
+function createNewNote(body, notesArray) {
+  const newNote = body;
+  if (!Array.isArray(notesArray))
+      notesArray = [];
+  
+  if (notesArray.length === 0)
+      notesArray.push(0);
+
+  body.id = notesArray[0];
+  notesArray[0]++;
+
+  notesArray.push(newNote);
+  fs.writeFileSync(
+      path.join(__dirname, '../../db/db.json'),
+      JSON.stringify(notesArray, null, 2)
+  );
+  return newNote;
+}
 
 module.exports = (app) => {
 
@@ -10,18 +30,8 @@ module.exports = (app) => {
     });
   
     app.post('/api/notes', (req, res) => {
-      let db = fs.readFileSync('db/db.json');
-      db = JSON.parse(db);
-      res.json(db);
-      let userNote = {
-        title: req.body.title,
-        text: req.body.text,
-        id: uniqid(),
-      };
-      db.push(userNote);
-      fs.writeFileSync('db/db.json', JSON.stringify(db));
-      res.json(db);
-  
+      const newNote = createNewNote(req.body, allNotes);
+      res.json(newNote);
     });
   
   
